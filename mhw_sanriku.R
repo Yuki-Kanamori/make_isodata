16706cb74a1d32c4c186763609008735c5c0f7db
Yuki-Kanamori <y.kanamory@gmail.com>
2025-12-19 09:46:53 +0900

    revised

--- mhw_sanriku.R
+++ mhw_sanriku.R
@@ -99,63 +99,149 @@ sst_ag = sst_ag %>% mutate(temp = paste(year, month, day, sep = "-")) %>% mutate
 sst_ak = sst_ak %>% mutate(temp = paste(year, month, day, sep = "-")) %>% mutate(t = as.Date(temp)) %>% select(-temp) %>% rename(temp = SST)
 sst_kg = sst_kg %>% mutate(temp = paste(year, month, day, sep = "-")) %>% mutate(t = as.Date(temp)) %>% select(-temp) %>% rename(temp = SST)
 all_sst = rbind(sst_mj, sst_or, sst_ag, sst_ak, sst_kg)
+head(all_sst)
+
+(lonlat_shore = all_sst %>% distinct(shore, .keep_all = TRUE))
+#   lat      ymd     lon temp year month day shore          t
+# 1 42.875 19820101 144.625  9.3 1982     1   1    MJ 1982-01-01
+# 2 43.125 19820101 144.875  8.9 1982     1   1    OR 1982-01-01
+# 3 43.125 19820101 144.875  8.9 1982     1   1    AG 1982-01-01
+# 4 42.875 19820101 144.875  9.7 1982     1   1    AK 1982-01-01
+# 5 43.125 19820101 145.125  9.4 1982     1   1    KG 1982-01-01
 
 shore = c("MJ", "OR", "AG", "AK", "KG")
 
-res_mhw = NULL
-for(i in shore){
-  temp = all_sst %>% filter(shore == i)
+# res_mhw = NULL
+# for(i in shore){
+#   temp = all_sst %>% filter(shore == i)
+#   
+#   res = detect_event(ts2clm(data = temp,
+#                             climatologyPeriod = c("1982-01-01", "2023-12-31")))
+#   cate = category(res, S = FALSE, name = "sanriku") %>% select(event_no, category, season)
+#   df_event = left_join(res[["event"]], cate, by = "event_no") %>% mutate(shore = paste(i))
+#   
+#   res_mhw = rbind(res_mhw, df_event)
+# }
+# unique(res_mhw$shore)
+
+res_events <- NULL
+
+for (i in unique(all_sst$shore)) {
   
-  res = detect_event(ts2clm(data = temp,
-                            climatologyPeriod = c("1982-01-01", "2023-12-31")))
-  cate = category(res, S = FALSE, name = "sanriku") %>% select(event_no, category, season)
-  df_event = left_join(res[["event"]], cate, by = "event_no") %>% mutate(shore = paste(i))
+  temp_i <- all_sst %>%
+    filter(shore == i) %>%
+    select(t, temp) %>%     # heatwaveR に必要な列だけ
+    arrange(t)
   
-  res_mhw = rbind(res_mhw, df_event)
+  # climatology（熱波/寒波共通で使う）
+  clim <- ts2clm(
+    data = temp_i,
+    climatologyPeriod = c("1982-01-01", "2023-12-31")
+  )
+  
+  # --- 熱波 ---
+  res_hw <- detect_event(clim)  # coldSpells=FALSE がデフォ
+  cate_hw <- category(res_hw, S = FALSE, name = "sanriku") %>%
+    select(event_no, category, season)
+  
+  ev_hw <- left_join(res_hw$event, cate_hw, by = "event_no") %>%
+    mutate(shore = i, event_type = "MHW")
+  
+  # --- 寒波（coldSpells） ---
+  res_cs <- detect_event(clim, coldSpells = TRUE)
+  cate_cs <- category(res_cs, S = FALSE, name = "sanriku") %>%
+    select(event_no, category, season)
+  
+  ev_cs <- left_join(res_cs$event, cate_cs, by = "event_no") %>%
+    mutate(shore = i, event_type = "MCS")
+  
+  res_events <- bind_rows(res_events, ev_hw, ev_cs)
 }
-unique(res_mhw$shore)
-summary(res_mhw)
-res_mhw = res_mhw %>% mutate(year = str_sub(date_peak, 1, 4))
 
-d = res_mhw %>% select(duration, shore, year) %>% rename(value = duration) %>% mutate(cate = "duration")
-i = res_mhw %>% select(intensity_mean, shore, year) %>% rename(value = intensity_mean) %>% mutate(cate = "intensity_mean")
-c = res_mhw %>% select(intensity_cumulative, shore, year) %>% rename(value = intensity_cumulative) %>% mutate(cate = "intensity_cumulative")
-dic = rbind(d, i, c) 
-summary(dic)
-fig_dic = dic %>% group_by(cate, year) %>% summarize(mean = mean(value))
-fig_dic$cate = factor(fig_dic$cate, levels = c("duration", "intensity_mean", "intensity_cumulative"))
-levels(fig_dic$cate)
+count(res_events, event_type)
+head(res_events)
 
-g = ggplot(fig_dic, aes(x = year, y = mean, group = cate))
-p = geom_point()
-l = geom_line()
-f = facet_wrap(~ cate, scales = "free", ncol = 3)
-labs = labs(x = "Year", y = "")
-th = theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 8))
-fig = g+p+l+f+labs+theme_bw()+th
-setwd(dir = "/Users/Yuki/Dropbox/isodata/sanriku/")
-ggsave(filename = "mhw.pdf", plot = fig, units = "in", width = 11.69, height = 8.27)
+summary(res_events)
+res_events = res_events %>% mutate(year = str_sub(date_peak, 1, 4))
 
-# 日数と強度の掛け算だけ
-g = ggplot(fig_dic %>% filter(cate == "intensity_cumulative") %>% mutate(year = as.numeric(year)), aes(x = year, y = mean, group = cate))
-p = geom_point()
-l = geom_line()
-labs = labs(x = "Year", y = "MHWs")
-th = theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 8))
-fig = g+p+l+labs+theme_bw()+th+scale_x_continuous(breaks = seq(1980, 2024, 10))
-setwd(dir = "/Users/Yuki/Dropbox/isodata/sanriku/")
-ggsave(filename = "mhw_intensity_cumulative.pdf", plot = fig, units = "in", width = 11.69, height = 8.27)
+res_year <- res_events %>%
+  group_by(year, event_type) %>%
+  summarise(
+    duration_mean = mean(duration, na.rm = TRUE),
+    intensity_mean = mean(intensity_mean, na.rm = TRUE),
+    intensity_cumulative = mean(intensity_cumulative, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+res_year2 <- res_year %>%
+  mutate(
+    year = as.integer(year),
+    event_type = factor(event_type, levels = c("MHW", "MCS"))
+  ) %>%
+  arrange(event_type, year)
+
+ggplot(res_year2,
+       aes(x = year, y = intensity_cumulative)) +
+  geom_line(color = "black", linewidth = 1) +
+  geom_point(size = 2) +
+  facet_wrap(~ event_type, ncol = 1,
+             labeller = labeller(
+               event_type = c(MHW = "Heatwaves", MCS = "Cold spells")
+             )) +
+  labs(
+    x = "Year"
+  ) +
+  theme_bw()
+
+res_year_shore <- res_events %>%
+  group_by(year, shore, event_type) %>%
+  summarise(
+    intensity_mean = mean(intensity_mean, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+res_year_shore2 <- res_year_shore %>%
+  mutate(
+    year = as.integer(year),
+    event_type = factor(event_type, levels = c("MHW", "MCS"))
+  ) %>%
+  arrange(event_type, year)
+
+ggplot(res_year_shore2,
+       aes(x = year, y = intensity_mean, color = shore)) +
+  geom_line() +
+  facet_wrap(~ event_type, ncol = 1) +
+  theme_bw()
+
+
+
+
 
 
 # 生物量と平均水温のデータにmhwsの強度*日数の値を付与する
-head(c); head(df)
-summary(c)
-c2 = c %>% group_by(year, shore) %>% summarize(sum = sum(value)) %>% mutate(year = as.numeric(year))
-summary(c2)
-temp = data.frame(shore = shore, year = rep(1985:2024, each = 5))
-c3 = left_join(temp, c2, by = c("year", "shore"))
-c3[is.na(c3)] = 0
-df = left_join(df, c3, by = c("year", "shore"))
+# 2) shore × year × event_type で intensity_cumulative を集計
+#    ここでは「その年に起きたイベントの intensity_cumulative を合計」しています
+evt_year <- res_events %>%
+  group_by(shore, year, event_type) %>%
+  summarise(
+    intensity_cumulative_sum = sum(intensity_cumulative, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+# 3) MHW/MCS を列にする（wide）
+evt_wide <- evt_year %>%
+  tidyr::pivot_wider(
+    names_from  = event_type,
+    values_from = intensity_cumulative_sum,
+    names_prefix = "icumu_",
+    values_fill = 0
+  )
+# できる列例：icumu_MHW, icumu_MCS
+evt_wide2 = evt_wide %>%
+  mutate(year = as.integer(year)) %>%
+  filter(between(year, 2002, 2023))
+
+df2 <- left_join(df, evt_wide2, by = c("shore", "year"))
 
 # 説明変数の標準化
 summary(df)
@@ -165,6 +251,8 @@ sum2 = data.frame(sum2 = scale(df$sum))
 df2 = cbind(df, m_sst2, sum2) %>% as.data.frame()
 head(df2)
 
+setwd(dir_iso)
+write.csv(df2, "df_boosting_sanriku.csv", fileEncoding = "CP932")
 
 
 # 各生物 --------------------------------------------
