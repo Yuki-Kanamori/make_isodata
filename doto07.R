02b4fcc16ca358b18c9c3b7ea44697deb394df8f
Yuki-Kanamori <y.kanamory@gmail.com>
2025-12-21 20:31:45 +0900

    revised

--- doto07.R
+++ doto07.R
@@ -50,7 +50,7 @@ head(df)
 df = df %>% mutate(height = as.numeric(str_sub(row, 2, 3)))
 
 setwd(dir_save)
-save(df, file = "sessile.Rdata")
+save(df, file = "sessile07.Rdata")
 
 head(df)
 df2 = df %>% group_by(year, plot, height, species) %>% count()
